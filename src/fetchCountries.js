f296f92c8d8e77a827da1db00b74791b6c1d2aa0
Darya <HavrylenkoIT@gmail.com>
2022-09-17 14:07:57 +0300

    экспорт

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export function fetchCountries(name) {
+ function fetchCountries(name) {
      
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
@@ -9,3 +9,4 @@ export function fetchCountries(name) {
        });
     
 }
+export {fetchCountries}
